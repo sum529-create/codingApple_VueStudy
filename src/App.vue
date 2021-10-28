a35ec5f52d2cef33f1f912b203142491e357d4fe
smnoh <smnoh@ui-lab.co.kr>
2021-10-28 13:22:32 +0900

    create_dataBinding_number count method

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="contents">
+    <section id="page">
+      <div class="page__room_sell">
+        <div class="menu">
+          <a v-for="(a,i) in navdata" :key="i">{{a}}</a>
+        </div>
+        <img src="@/assets/logo.png" alt="logo">
+        <div class="txt_area" v-for="(item, i) in name" :key="i">
+          <h4>{{item}}원룸</h4>
+          <p :style="fs">{{i+1}}0만원</p>
+          <button @click="addNum(i)">신고수</button><br/>
+          <span>{{num[i]}}</span>
+        </div>
+      </div>
+    </section>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      /* HTML속성도 데이터 바인딩 가능함 */
+      fs: 'color:red',
+      num:[], // 신고수
+      navdata:['Home', 'Shop', 'About'],
+      name:['역삼동', '천호동', '마포구'],
+    }
+  },
+  mounted() {
+    for(let i=0; i<this.navdata.length; i++){
+      this.num.push(0);
+    }
+  },
+  methods:{
+    addNum(i){
+      this.num[i] += 1;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .contents{
+    position: relative;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .menu {
+    background: rgb(4, 92, 73);
+    padding: 15px;
+    border-radius: 5px;
+  }
+  .menu a{
+    color: #FFF;
+    padding: 10px;
+  }
 </style>
---
+++ src/DataBinding_01.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img src="@assets/logo.png" alt="">
+    <h4>xx 원룸</h4>
+    <p :style="fs">{{price}}만원</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      /* HTML속성도 데이터 바인딩 가능함 */
+      price: 50,
+      fs: 'color:red',
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
